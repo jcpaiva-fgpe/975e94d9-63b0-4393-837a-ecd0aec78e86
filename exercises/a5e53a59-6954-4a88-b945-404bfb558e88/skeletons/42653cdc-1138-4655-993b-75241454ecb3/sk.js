5f262f561f433de27f283544b201fb6e6d9df21a
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-13 22:39:27 +0100

    updated file exercises/a5e53a59-6954-4a88-b945-404bfb558e88/skeletons/42653cdc-1138-4655-993b-75241454ecb3/sk.js

--- exercises/a5e53a59-6954-4a88-b945-404bfb558e88/skeletons/42653cdc-1138-4655-993b-75241454ecb3/sk.js
+++ exercises/a5e53a59-6954-4a88-b945-404bfb558e88/skeletons/42653cdc-1138-4655-993b-75241454ecb3/sk.js
@@ -1,7 +1,8 @@
-var first = 3;
-var second = 2;
-// Don't touch code above this line!
+// Don't touch anything, just add the console.log somewhere inside! :)
+function main() {
+    var first = 3;
+    var second = 2;
 
-
-var sum = first + second;
-console.log("Sum: " + sum);
+    var sum = first + second;
+    console.log("Sum: " + sum);
+}
