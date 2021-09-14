f266611f6027d5acf7acee5035e6ea12182c0468
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-14 11:01:55 +0100

    updated file exercises/a5e53a59-6954-4a88-b945-404bfb558e88/solutions/bb08af6b-a5c6-493b-a4d0-43bc4b7d92cb/so.js

--- exercises/a5e53a59-6954-4a88-b945-404bfb558e88/solutions/bb08af6b-a5c6-493b-a4d0-43bc4b7d92cb/so.js
+++ exercises/a5e53a59-6954-4a88-b945-404bfb558e88/solutions/bb08af6b-a5c6-493b-a4d0-43bc4b7d92cb/so.js
@@ -1,7 +1,9 @@
-var first = 3;
-var second = 2;
-// Don't touch code above this line!
-console.log(second);
+function main() {
+    var first = 3;
+    var second = 2;
+    // Don't touch code above this line!
+    console.log(second);
 
-var sum = first + second;
-console.log("Sum: " + sum);
+    var sum = first + second;
+    console.log("Sum: " + sum);
+}
