1a08c317ac8c4af3ce17d2cc8713177e8c398a88
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-14 16:00:13 +0100

    created file exercises/c8126a4b-f38e-476e-81d8-271a00f2a88d/skeletons/91d7f47d-50c5-4f89-9961-b18f3c94d960/s.js

---
+++ exercises/c8126a4b-f38e-476e-81d8-271a00f2a88d/skeletons/91d7f47d-50c5-4f89-9961-b18f3c94d960/s.js
@@ -0,0 +1,11 @@
+function main() {
+    var FAVORITE_SONG = 'Careless Whisper';
+
+    // Change code above this line
+    try {      
+        FAVORITE_SONG = 'Careless Whisper'
+        console.log("My favorite song is " + FAVORITE_SONG);
+    } catch(err) {
+        console.log("Someone has tried to change my favorite song!")
+    }
+}
