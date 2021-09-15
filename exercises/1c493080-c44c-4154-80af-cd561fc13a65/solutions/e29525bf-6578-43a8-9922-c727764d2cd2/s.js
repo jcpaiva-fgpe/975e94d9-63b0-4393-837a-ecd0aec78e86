3d8ae44f09fdd05e24d54f49e8e5ce508f6752bc
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-15 13:21:07 +0100

    created file exercises/1c493080-c44c-4154-80af-cd561fc13a65/solutions/e29525bf-6578-43a8-9922-c727764d2cd2/s.js

---
+++ exercises/1c493080-c44c-4154-80af-cd561fc13a65/solutions/e29525bf-6578-43a8-9922-c727764d2cd2/s.js
@@ -0,0 +1,9 @@
+function main() {
+    const step = "step";
+    const stepLength = 1;
+    const small = "small";
+    const start = "This is";
+    const forAman = "for a man,";
+    const rest = "one giant leap for mankind";
+    console.log(start, stepLength, small, step, forAman, rest);
+}
