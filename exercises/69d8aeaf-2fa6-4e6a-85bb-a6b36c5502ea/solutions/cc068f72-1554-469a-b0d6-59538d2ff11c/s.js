a075b09a43a62470486daa0a5d7f555595fe8439
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-15 16:07:35 +0100

    created file exercises/69d8aeaf-2fa6-4e6a-85bb-a6b36c5502ea/solutions/cc068f72-1554-469a-b0d6-59538d2ff11c/s.js

---
+++ exercises/69d8aeaf-2fa6-4e6a-85bb-a6b36c5502ea/solutions/cc068f72-1554-469a-b0d6-59538d2ff11c/s.js
@@ -0,0 +1,10 @@
+function main() {
+    console.log("Please enter a number:");
+    x = readln();
+
+    if(10 < x < 100 && x % 3 == 0) {
+        console.log('Matches both conditions.');
+    } else {
+        console.log('Does not match both conditions at once.');
+    }    
+}
