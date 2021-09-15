15eb86a28d4fb4eed72d87eb51a976b6c9f19036
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-15 17:49:16 +0100

    created file exercises/3c8b00d4-b4c1-485f-aa85-0fde21024e15/skeletons/69e19a04-15d7-4472-b8ae-90b94cc63711/s.js

---
+++ exercises/3c8b00d4-b4c1-485f-aa85-0fde21024e15/skeletons/69e19a04-15d7-4472-b8ae-90b94cc63711/s.js
@@ -0,0 +1,10 @@
+function main() {
+    console.log('Enter the secret lunch code:')
+    code = readln();
+
+    if(code == 'Yummy!') {
+        console.log('That is a good one!')
+    } else {
+        console.log('That is not good.')
+    }
+}
