aed780cf75fb973a781066106550dde323c49cce
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-15 17:40:35 +0100

    created file exercises/3fcbcaa6-455f-4ec0-b4b9-bb84541ff8e1/solutions/44390328-c82b-4ef5-b0c9-0fa2ff0b2f54/s.js

---
+++ exercises/3fcbcaa6-455f-4ec0-b4b9-bb84541ff8e1/solutions/44390328-c82b-4ef5-b0c9-0fa2ff0b2f54/s.js
@@ -0,0 +1,10 @@
+function main() {
+    for(let i = 0; i < 7; i++) {
+        let line = "*";
+        for(let x = 0; x < i; x++) {
+            line = line + "*";
+            
+        }
+        console.log(line);
+    }
+}
