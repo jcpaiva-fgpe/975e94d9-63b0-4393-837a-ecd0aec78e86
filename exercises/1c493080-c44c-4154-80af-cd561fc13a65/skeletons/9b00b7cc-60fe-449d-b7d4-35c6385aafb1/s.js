32476415edd9d777da6d3bc51ef53499ec63dd4a
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-15 13:18:22 +0100

    created file exercises/1c493080-c44c-4154-80af-cd561fc13a65/skeletons/9b00b7cc-60fe-449d-b7d4-35c6385aafb1/s.js

---
+++ exercises/1c493080-c44c-4154-80af-cd561fc13a65/skeletons/9b00b7cc-60fe-449d-b7d4-35c6385aafb1/s.js
@@ -0,0 +1,9 @@
+function main() {
+    var S = "step";
+    var 1step = 1;
+    var s = "small";
+    var ha ha = "That's a";
+    var console.log = "for a man,";
+    var String = "one giant leap for manking";
+    console.log(console.log + ha ha, 1step, s, String);
+}
