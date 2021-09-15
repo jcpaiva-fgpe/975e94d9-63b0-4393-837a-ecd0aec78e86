ff67c13c8d9d6cec10356965dba827dbdc433ad1
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-15 17:42:03 +0100

    updated file exercises/3fcbcaa6-455f-4ec0-b4b9-bb84541ff8e1/skeletons/6f043474-e78f-4d2c-9416-e0f47e34c3d5/s.js

--- exercises/3fcbcaa6-455f-4ec0-b4b9-bb84541ff8e1/skeletons/6f043474-e78f-4d2c-9416-e0f47e34c3d5/s.js
+++ exercises/3fcbcaa6-455f-4ec0-b4b9-bb84541ff8e1/skeletons/6f043474-e78f-4d2c-9416-e0f47e34c3d5/s.js
@@ -1,10 +1,9 @@
 function main() {
     for(let i = 0; i < 7; i++) {
         let line = "*";
-        for(let x = 0; x < i; x++) {
+        for(let x = 0; x < 7; x++) {
             line = line + "*";
-            
+            console.log(line); // move to new line
         }
-        console.log(line);
     }
 }
