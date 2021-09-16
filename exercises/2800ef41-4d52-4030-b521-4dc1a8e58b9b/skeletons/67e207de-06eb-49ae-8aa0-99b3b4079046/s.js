45e1d2c50a5081f41249ac06ae20ae3ea624fe96
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-16 13:47:57 +0100

    created file exercises/2800ef41-4d52-4030-b521-4dc1a8e58b9b/skeletons/67e207de-06eb-49ae-8aa0-99b3b4079046/s.js

---
+++ exercises/2800ef41-4d52-4030-b521-4dc1a8e58b9b/skeletons/67e207de-06eb-49ae-8aa0-99b3b4079046/s.js
@@ -0,0 +1,6 @@
+function main() {
+    let myArray = {};
+
+    // This code checks if your array is multi-dimensional. Please, change the code above.
+    console.log(myArray.every(item => Array.isArray(item))); // Returns true if myArray is multi-dimensional
+}
