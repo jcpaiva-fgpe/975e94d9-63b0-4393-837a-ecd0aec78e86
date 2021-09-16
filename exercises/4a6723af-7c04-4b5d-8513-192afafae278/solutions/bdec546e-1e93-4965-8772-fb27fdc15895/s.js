72b45312a9434e59ba4b32103d4a81ea6e7b6b22
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-16 14:18:14 +0100

    created file exercises/4a6723af-7c04-4b5d-8513-192afafae278/solutions/bdec546e-1e93-4965-8772-fb27fdc15895/s.js

---
+++ exercises/4a6723af-7c04-4b5d-8513-192afafae278/solutions/bdec546e-1e93-4965-8772-fb27fdc15895/s.js
@@ -0,0 +1,9 @@
+function main() {
+    let myArray = [1, 2, 3, 4];
+    // Only change code below this line;
+
+    myArray[1] = 15;
+
+    // Only change code above this line
+    console.log(myArray[1]);
+}
