bc96aa137d366abc58c6dd9919d3b679e04b8f5c
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-16 13:55:19 +0100

    created file exercises/b8f302d8-cfb9-46c6-be86-4be2bfcbd22d/solutions/3bdfe1d9-12b2-448f-8364-71e663700fee/s.js

---
+++ exercises/b8f302d8-cfb9-46c6-be86-4be2bfcbd22d/solutions/3bdfe1d9-12b2-448f-8364-71e663700fee/s.js
@@ -0,0 +1,9 @@
+function main() {
+    let myArray = [[432, 43242], [423842, 432], [32422, 432]];
+    // Change code below this line;
+
+    let myData = myArray[1][0];
+
+    // Change code above this line;
+    console.log(myData);
+}
