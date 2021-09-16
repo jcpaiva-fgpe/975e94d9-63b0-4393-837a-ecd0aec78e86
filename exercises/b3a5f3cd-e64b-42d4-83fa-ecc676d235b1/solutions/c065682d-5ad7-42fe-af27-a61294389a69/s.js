21fbb22e1384d8f143daf129f7cb1e77b53d8ccc
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-16 14:29:41 +0100

    updated file exercises/b3a5f3cd-e64b-42d4-83fa-ecc676d235b1/solutions/c065682d-5ad7-42fe-af27-a61294389a69/s.js

---
+++ exercises/b3a5f3cd-e64b-42d4-83fa-ecc676d235b1/solutions/c065682d-5ad7-42fe-af27-a61294389a69/s.js
@@ -0,0 +1,9 @@
+function main() {
+    let myArray = [["Servus", "Bis bald"], ["Hola", "Chau"]];
+    // Change code below this line
+
+    myArray.push(["Hi", "Bye"]);
+
+    // Change code above this line
+    console.log(myArray[2][1]);
+}
