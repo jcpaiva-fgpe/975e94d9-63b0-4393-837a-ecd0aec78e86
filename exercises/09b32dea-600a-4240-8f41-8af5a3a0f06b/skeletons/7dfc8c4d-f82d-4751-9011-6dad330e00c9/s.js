e103a49b591d0ec4a6c72051f67ba9e9c78e7975
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-17 12:55:46 +0100

    created file exercises/09b32dea-600a-4240-8f41-8af5a3a0f06b/skeletons/7dfc8c4d-f82d-4751-9011-6dad330e00c9/s.js

---
+++ exercises/09b32dea-600a-4240-8f41-8af5a3a0f06b/skeletons/7dfc8c4d-f82d-4751-9011-6dad330e00c9/s.js
@@ -0,0 +1,8 @@
+function main() {
+    let myArray = [["adsada", 1], ["Hi", 2], ["Hey", 3]];
+    // Add code below this line
+
+
+    // Add code above this line
+    console.log(myArray[0].toString())
+}
