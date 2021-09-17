dd66632b13cedc5a7ee3a46cdd1385e182be9483
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-17 12:46:36 +0100

    created file exercises/1577442c-3f8d-4321-b552-95d7cf8f0e61/skeletons/769c3df1-bd62-43e7-a66a-d19b0e8dc3b6/s.js

---
+++ exercises/1577442c-3f8d-4321-b552-95d7cf8f0e61/skeletons/769c3df1-bd62-43e7-a66a-d19b0e8dc3b6/s.js
@@ -0,0 +1,9 @@
+function main() {
+    let myArray = [[1, "John"], [2, "Robert"], [3, "Joanna"], [4, "Gabi"]];
+    // Change code below this line
+
+    let removedElement;
+
+    // Change code above this line
+    console.log(removedElement);
+}
