73af6d8cb0bb898b00db66cdd916f07e2ffdcca5
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-20 22:03:19 +0100

    updated file exercises/e54a9b6a-fd09-4a39-bfab-22fb2e2dd735/skeletons/2c2d51b5-0858-48fd-b153-44dc10eedb81/s.js

--- exercises/e54a9b6a-fd09-4a39-bfab-22fb2e2dd735/skeletons/2c2d51b5-0858-48fd-b153-44dc10eedb81/s.js
+++ exercises/e54a9b6a-fd09-4a39-bfab-22fb2e2dd735/skeletons/2c2d51b5-0858-48fd-b153-44dc10eedb81/s.js
@@ -2,6 +2,8 @@ function main() {
     let myAmazingString = "Jello!";
     // Change code below this line;
 
+    myAmazingString[0] = "H"; // Change this very line
+
     // Change code above this line
     console.log(`${myAmazingString} Nice to meet you!`);
 }
