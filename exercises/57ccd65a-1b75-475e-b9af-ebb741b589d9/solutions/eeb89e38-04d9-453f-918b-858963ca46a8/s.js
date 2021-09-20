82f13d1d4fad118a02d8813c1753fe948a833469
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-20 23:02:31 +0100

    created file exercises/57ccd65a-1b75-475e-b9af-ebb741b589d9/solutions/eeb89e38-04d9-453f-918b-858963ca46a8/s.js

---
+++ exercises/57ccd65a-1b75-475e-b9af-ebb741b589d9/solutions/eeb89e38-04d9-453f-918b-858963ca46a8/s.js
@@ -0,0 +1,15 @@
+function main() {
+    let amazingObject = {
+        prop1: 423,
+        prop2: 213,
+        prop3: 222,
+        prop4: {
+            prop1: 11
+        }
+    };
+
+    console.log(amazingObject.prop1);
+    console.log(amazingObject.prop2);
+    console.log(amazingObject.prop3);
+    console.log(amazingObject.prop4.prop1);
+}
