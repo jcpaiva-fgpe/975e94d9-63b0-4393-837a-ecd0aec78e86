0208cb49359810f5e85dc89de1240e8917a41a2a
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-20 23:13:07 +0100

    created file exercises/66599f6f-9a6e-4839-a0ac-1fe210978ebb/solutions/94545046-f182-4187-a86a-0df0de2fb065/s.js

---
+++ exercises/66599f6f-9a6e-4839-a0ac-1fe210978ebb/solutions/94545046-f182-4187-a86a-0df0de2fb065/s.js
@@ -0,0 +1,9 @@
+function main() {
+    let amazingObj = {
+        "huge number": 1000000,
+        "small number": 10
+    }
+
+    console.log(amazingObj["huge number"]);
+    console.log(amazingObj["small number"]);
+}
