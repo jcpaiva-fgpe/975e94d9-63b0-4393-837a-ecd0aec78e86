8e55b0ae0104319fda981e64b5ee4fe92a97e68b
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-20 22:03:47 +0100

    created file exercises/e54a9b6a-fd09-4a39-bfab-22fb2e2dd735/solutions/5e4202ef-a5bc-48ac-9bfd-3642a782e0d6/s.js

---
+++ exercises/e54a9b6a-fd09-4a39-bfab-22fb2e2dd735/solutions/5e4202ef-a5bc-48ac-9bfd-3642a782e0d6/s.js
@@ -0,0 +1,9 @@
+function main() {
+    let myAmazingString = "Jello!";
+    // Change code below this line;
+
+    myAmazingString = "Hello!";
+
+    // Change code above this line
+    console.log(`${myAmazingString} Nice to meet you!`);
+}
