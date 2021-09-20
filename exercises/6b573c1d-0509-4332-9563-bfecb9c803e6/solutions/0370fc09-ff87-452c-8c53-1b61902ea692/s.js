2c171a02635e2c03c75fdd60209c1d3939257c1e
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-20 22:46:07 +0100

    updated file exercises/6b573c1d-0509-4332-9563-bfecb9c803e6/solutions/0370fc09-ff87-452c-8c53-1b61902ea692/s.js

--- exercises/6b573c1d-0509-4332-9563-bfecb9c803e6/solutions/0370fc09-ff87-452c-8c53-1b61902ea692/s.js
+++ exercises/6b573c1d-0509-4332-9563-bfecb9c803e6/solutions/0370fc09-ff87-452c-8c53-1b61902ea692/s.js
@@ -1,1 +1,12 @@
-function main()
+function main() {
+    let myDog = {
+    // Only change code below this line
+        legs: 4,
+        tails: 1,
+        friends: ["cats"]
+
+    // Only change code above this line
+    };
+
+    console.log(Object.keys(myDog));
+}
