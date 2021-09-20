21f68185aa9d517a43512a8f882df0e82f347296
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-20 22:53:56 +0100

    created file exercises/57ccd65a-1b75-475e-b9af-ebb741b589d9/skeletons/59bf8f5e-3ef2-408b-9767-bddb732a061a/s.js

---
+++ exercises/57ccd65a-1b75-475e-b9af-ebb741b589d9/skeletons/59bf8f5e-3ef2-408b-9767-bddb732a061a/s.js
@@ -0,0 +1,12 @@
+function main() {
+    let amazingObject = {
+        prop1: 423,
+        prop2: 213,
+        prop3: 222,
+        prop4: {
+            prop1: 11
+        }
+    };
+
+    console.log(amazingObject);
+}
