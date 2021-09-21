03eb052cc1faee812fc8d9f1e9ba6e630a7d2b14
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-21 23:02:49 +0100

    created file exercises/fff721e8-881b-44ba-9968-abd8d75b4b7d/solutions/6efdd322-805c-43c7-aaff-16978750961f/s.js

---
+++ exercises/fff721e8-881b-44ba-9968-abd8d75b4b7d/solutions/6efdd322-805c-43c7-aaff-16978750961f/s.js
@@ -0,0 +1,13 @@
+function main() {
+    let myObject = {
+        isCool: false,
+        isGreat: true,
+        isAmazing: true,
+        isAwful: false
+    }
+
+    myObject.isCool = true;
+
+    // Change code above this line
+    console.log(myObject.isCool);
+}
