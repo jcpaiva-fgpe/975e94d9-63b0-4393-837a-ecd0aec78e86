3046800e53b193f2c59686beb679dbbfd04669bb
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-21 23:07:37 +0100

    updated file exercises/5414733a-96ab-4791-bdd2-39d2e601da1c/solutions/cb637dde-b77a-41a5-aea3-c12fc2f41fa9/s.js

---
+++ exercises/5414733a-96ab-4791-bdd2-39d2e601da1c/solutions/cb637dde-b77a-41a5-aea3-c12fc2f41fa9/s.js
@@ -0,0 +1,13 @@
+function main() {
+    let test = {
+        level: 3,
+        min: 7.5,
+    }
+
+    // Change code below this line
+
+    test.max = 10;
+
+    // Change code above this line
+    console.log(test.max);
+}
