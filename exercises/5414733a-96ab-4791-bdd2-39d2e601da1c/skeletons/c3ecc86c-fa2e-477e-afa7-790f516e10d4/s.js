def28d742f046c53ef34ad8aff4a3d607ed7b5ae
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-21 23:06:04 +0100

    created file exercises/5414733a-96ab-4791-bdd2-39d2e601da1c/skeletons/c3ecc86c-fa2e-477e-afa7-790f516e10d4/s.js

---
+++ exercises/5414733a-96ab-4791-bdd2-39d2e601da1c/skeletons/c3ecc86c-fa2e-477e-afa7-790f516e10d4/s.js
@@ -0,0 +1,12 @@
+function main() {
+    let test = {
+        level: 3,
+        min: 7.5,
+    }
+
+    // Change code below this line
+
+
+    // Change code above this line
+    console.log(test);
+}
