292a31d7ecb0551845505a2f18397cbc1c1318e1
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-21 23:21:26 +0100

    created file exercises/e887ba5c-eeeb-4695-a689-05161982cec2/solutions/dfc3c922-40c9-493f-b0ea-8b8092670881/s.js

---
+++ exercises/e887ba5c-eeeb-4695-a689-05161982cec2/solutions/dfc3c922-40c9-493f-b0ea-8b8092670881/s.js
@@ -0,0 +1,12 @@
+function main() {
+    let myObject = {
+        length: 12,
+        max: 13,
+        test: 2
+    }
+
+    let propertyName = readln(); // This variable stores name of the property the user is looking for
+
+    // Change code below this line
+    console.log(myObject.hasOwnProperty(propertyName) ? myObject[propertyName] : ``);
+}
