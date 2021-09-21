07efeb3a3dfe8ec5a948297bc601dd21f6c37941
Filip Miernik <mrnkfelipe@gmail.com>
2021-09-21 23:22:09 +0100

    updated file exercises/e887ba5c-eeeb-4695-a689-05161982cec2/solutions/dfc3c922-40c9-493f-b0ea-8b8092670881/s.js

--- exercises/e887ba5c-eeeb-4695-a689-05161982cec2/solutions/dfc3c922-40c9-493f-b0ea-8b8092670881/s.js
+++ exercises/e887ba5c-eeeb-4695-a689-05161982cec2/solutions/dfc3c922-40c9-493f-b0ea-8b8092670881/s.js
@@ -8,5 +8,5 @@ function main() {
     let propertyName = readln(); // This variable stores name of the property the user is looking for
 
     // Change code below this line
-    console.log(myObject.hasOwnProperty(propertyName) ? myObject[propertyName] : ``);
+    console.log(myObject.hasOwnProperty(propertyName) ? myObject[propertyName] : `This object does not have a property named ${propertyName}`);
 }
